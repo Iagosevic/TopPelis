f37557590066891def72d92f18234ce95d91e5f9
Iagosevic <micromachine13@hotmail.com>
2021-05-16 13:53:05 +0200

    Add files via upload

--- handlers/comentarios/add.py
+++ handlers/comentarios/add.py
@@ -1,42 +1,48 @@
 # coding: utf-8
-# Añade un nuevo comentario
+# Handler para añadir un nuevo comentario
 
+#Librerias importadas
 import webapp2
 from webapp2_extras import jinja2
 from webapp2_extras.users import users
 from google.appengine.ext import ndb
 from model.comentario import Comentario
-from model.pelicula import Pelicula
-from datetime import datetime
 
 class ComentarioAddHandler(webapp2.RequestHandler):
+    #Datos que obtenemos desde el showall
     def get(self):
+        #Usuario conectado actualmente
         usuario = users.get_current_user()
+        #Recuperamos la pelicula en la que vamos a añadir el comentario
         tupla = Comentario.recupera_para(self.request)
         pelicula = tupla[0]
-        #comentarios = tupla[1]
+        #Datos que pasamos a la vista de añadir comentario
         valores_plantilla = {
             "usr" : usuario,
             "pelicula" : pelicula
         }
-
+        #Instancia de jinja2 para cargar la vista
         jinja = jinja2.get_jinja2(app=self.app)
         self.response.write(jinja.render_template("comentarios_add.html",**valores_plantilla))
 
+    #Datos que obtemos del form para añdir comentario
     def post(self):
-        #pelicula = Pelicula.recupera(self.request)
-        #titulo = self.request.GET["id"]
+        #Key de la pelicula
         titulo = self.request.GET["id"]
+        #Usuario conectado
         login = users.get_current_user().nickname()
+        #Comentario que ha introducido en el form
         comentario = self.request.get("edComentario")
-        #now = datetime.now()
-        #hora = now.time()
 
+        #Creamos el comentario para añadirlo al Datastore
         comentario = Comentario(titulo=ndb.Key(urlsafe=titulo), login=login, comentario=comentario)
+        #Añadimos el comentario
         comentario.put()
 
+        #Nos redirecciona al index
         return self.redirect("/")
 
+#Indicamos el handler
 app = webapp2.WSGIApplication([
     ('/comentarios/add', ComentarioAddHandler)
 ], debug=True)
--- handlers/comentarios/showall.py
+++ handlers/comentarios/showall.py
@@ -1,27 +1,35 @@
+# coding: utf-8
+# Handler para ver todos los comentarios de una pelicula
+
+#Librerias importadas
 import webapp2
 from webapp2_extras import jinja2
 from webapp2_extras.users import users
-
 from model.comentario import Comentario
-from google.appengine.ext import ndb
+
 
 class ComentariosShowallHandler(webapp2.RequestHandler):
+    # Datos que obtenemos del index
     def get(self):
+        #Usuario conectado
         usuario = users.get_current_user()
+        #Pelicula de la que queremos ver sus comentarios y todos los comentarios de la misma
         tupla = Comentario.recupera_para(self.request)
         pelicula = tupla[0]
         comentarios = tupla[1]
 
+        #Datos que pasamos a la vista de ver comentarios
         valores_plantilla = {
             "usr": usuario,
             "comentarios": comentarios,
             "pelicula": pelicula
         }
 
+        # Instancia de jinja2 para cargar la vista
         jinja = jinja2.get_jinja2(app=self.app)
         self.response.write(jinja.render_template("comentarios_showall.html",**valores_plantilla))
 
-
+#Indicamos el handler
 app = webapp2.WSGIApplication([
     ('/comentarios/showall', ComentariosShowallHandler)
 ], debug=True)
